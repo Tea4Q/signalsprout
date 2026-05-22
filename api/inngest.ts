13a013ea867548be5610ea3e49b25b48901e4e75
Tea4Q <ChandraSkinner@gmail.com>
2026-05-22 16:38:14 -0500

    fix: self-contained api/inngest.mjs with no local ts imports

--- api/inngest.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/node";
-import { inngest } from "../inngest/client";
-import { publishScheduledPost } from "../inngest/functions/publish-post";
-import { trackPublishedPost } from "../inngest/functions/track-post";
-
-export default serve({
-  client: inngest,
-  functions: [publishScheduledPost, trackPublishedPost],
-});
